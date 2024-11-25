b3aebea34bb138043f528a1036b5ccf45fd523ba
David Robert <contact@david-robert.fr>
2024-11-25 15:16:06 +0100

    render twig in controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', 'home')]
     public function home() {
-        return new Response('<h1>Page Accueil</h1>');
+        // la méthode render de la classe AbstractController
+        // récupère le fichier twig passé en parametre
+        // dans le dossier template
+        // elle le convertit en HTML
+        // elle créé une réponse HTTP valide
+        // avec en status HTTP 200
+        // et en body, le HTML généré
+        return $this->render('home.html.twig');
     }
 
 }
--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -12,24 +12,15 @@ class PokerController
     #[Route('/poker', 'poker')]
     public function poker()
     {
-
-        // appelle la méthode createFromGlobals
-        // sans avoir besoin de faire l'instance
-        // de classe manuellement
-        // cette méthode permet de remplir
-        // la variable $request avec toutes
-        // les données de requête (GET, POST, SESSION, addresse IP etc)
         $request = Request::createFromGlobals();
-        // j'utilise la propriété query, qui me
-        // permet de récupérer les données GET
         $age = $request->query->get('age');
 
+        if ($age >= 18) {
+            return new Response('<p>Bienvenue sur le site de Poker en ligne</p>');
+        } else {
+            return new Response('Dégage morveux');
+        }
 
-        var_dump($age); die;
-
-
-
-        return new Response('Bienvenue sur le site de Poker en ligne');
     }
 
 }
