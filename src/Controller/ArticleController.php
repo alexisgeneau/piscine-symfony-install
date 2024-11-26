9dfc6d233bb5cf8572e78aef86902f7c5105ad23
David Robert <contact@david-robert.fr>
2024-11-26 15:43:51 +0100

    filter date twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -66,30 +66,35 @@ class ArticleController extends AbstractController
                 'title' => 'Article 1',
                 'content' => 'Content of article 1',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 2,
                 'title' => 'Article 2',
                 'content' => 'Content of article 2',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 3,
                 'title' => 'Article 3',
                 'content' => 'Content of article 3',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 4,
                 'title' => 'Article 4',
                 'content' => 'Content of article 4',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 5,
                 'title' => 'Article 5',
                 'content' => 'Content of article 5',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ]
 
         ];
